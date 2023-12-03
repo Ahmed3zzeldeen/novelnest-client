eb19639d081b0fd814378dc518d8c0f646f69355
Ahmed M. Ezzeldeen <73358147+Ahmed3zzeldeen@users.noreply.github.com>
2023-12-03 21:34:39 +0200

    Feature/create-a-header (#1)
    * Add react-router-dom to our project
    
    * Add Header component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import * as ROUTES from './Constants/routes.js';
+import { Home } from './Pages/index.js';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
+const theme = createTheme({
+  palette: {
+    dark: {
+      main: '#000000',
+    },
+  },
+  typography: {
+    fontFamily: [
+      'Roboto',
+      'sans-serif',
+    ].join(','),
+  },
+});
+
 function App() {
   return (
-    <>
-      <h1>Welcome to our website</h1>
-      <p>Happy Hacking!</p>
-    </>
+    <ThemeProvider theme={theme}>
+      <Router>
+        <Routes>
+          <Route path={ROUTES.LANDING} element={<Home />} >
+            <Route path={ROUTES.HOME} element={<Home />} />
+          </Route>
+        </Routes>
+      </Router>
+    </ThemeProvider>
   );
 }
 
---
+++ src/Constants/routes.js
@@ -0,0 +1,8 @@
+export const LANDING = '/';
+export const HOME = '/home';
+export const BOOKS = '/books';
+export const ABOUT_US = '/about-us';
+export const CONTACT = '/contact';
+export const SING_UP = '/Sign-Up';
+export const LOG_IN = '/log-in';
+
